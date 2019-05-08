050c8e2d6ca8fe89b4259ac5cbb83083276db609
theSoenke <soenke.behrendt@gmail.com>
2019-05-08 20:09:59 +0200

    Implement model architecture

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 
 from data_handler import DataHandler
+from model import build_model
 
 epochs = 10
 learning_rate = 1e-3
@@ -25,7 +26,6 @@ def build_network():
 
     return model
 
-
 data_loader = DataHandler(
     batch_size=batch_size,
     data_version='kitti2015',
@@ -36,3 +36,13 @@ data_loader = DataHandler(
     num_val_img=40,
 )
 data_loader.load()
+
+model = build_model(left_input_shape=data_loader.l_psz, right_input_shape=data_loader.r_psz)
+train_samples = data_loader.pixel_loc.shape[0]
+
+model.compile(loss="categorical_crossentropy", optimizer=Adam(lr=learning_rate), metrics=["accuracy"])
+model.fit_generator(
+    generator=data_loader.generator,
+    steps_per_epoch=train_samples // batch_size,
+    epochs=10,
+)
