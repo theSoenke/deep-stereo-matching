FIx accuracy calculation

utils.py
@@ -12,14 +12,9 @@ def loss_function(pred, target, weights):
 
 
 def pixel_accuracy(pred, target, pixel=3):
-    pred, _ = pred.max(dim=1)
-    pred = pred.abs()
-    return pred[pred <= pixel].shape[0] / pred.size(0)
-
-
-def pixel_accuracy2(pred, target, pixel=3):
     _, indices = pred.max(dim=1)
+    target = target.squeeze(dim=1)
     target = target.type(torch.long)
     pred = (indices-target).abs()
     batch_size = pred.size(0)
-    return pred[pred <= pixel].sum() / batch_size
+    return pred[pred <= pixel].shape[0] / batch_size